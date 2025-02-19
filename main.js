913c474e93d9880806af3c1283fd7dbde0ebf0f4
alessandrofalcoo <alefalco.1003@gmail.com>
2025-02-19 16:29:07 +0100

    fix variables scope

--- main.js
+++ main.js
@@ -1,23 +1,26 @@
 const nameEl = document.getElementById('name');
 const kmEl = Number(document.getElementById('km').value);
-const ticketPrice = 0.21 * kmEl;
-const underAge_Discount = (ticketPrice / 100) * 20;
-const overAge_Discount = (ticketPrice / 100) * 40;
-const underAge_Ticket = ticketPrice - underAge_Discount;
-const over_Ticket = ticketPrice - overAge_Discount;
-let selectAge = document.getElementById('age');
-let overAge = document.getElementById('overage').value;
-const underage = document.getElementById('underage').value;
-const over = document.getElementById('over_sixtyFive').value;
+
 
 const generate = document.querySelector('button')
+
 generate.addEventListener('click', function () {
 
-    if (selectAge == over) {
+    const ticketPrice = 0.21 * kmEl;
+    const underAge_Discount = (ticketPrice / 100) * 20;
+    const overAge_Discount = (ticketPrice / 100) * 40;
+    const underAge_Ticket = ticketPrice - underAge_Discount;
+    const over_Ticket = ticketPrice - overAge_Discount;
+    let selectAge = document.getElementById('age').value;
+    let overAge = document.getElementById('overage').value;
+    let underage = document.getElementById('underage').value;
+    let over = document.getElementById('over_sixtyFive').value;
+
+    if (selectAge = over) {
         console.log(over_Ticket);
-    } else if (selectAge == underage) {
+    } else if (selectAge = underage) {
         console.log(underAge_Ticket);
-    } else if (selectAge == overAge) {
+    } else if (selectAge = overAge) {
         console.log(ticketPrice);
 
     }
