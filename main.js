8a15191dbdf8d3326c6afad803e56f50f68a28de
alessandrofalcoo <alefalco.1003@gmail.com>
2025-02-19 15:41:26 +0100

    add if statement for calculating the ticket price add const to select html elements

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
 const nameEl = document.getElementById('name');
 const kmEl = document.getElementById('km');
 const ticketPrice = 0.21 * kmEl;
-const underAge_Discount = (ticketPrice/100) * 20;
-const overAge_Discount = (ticketPrice/100) * 40;
+console.log(ticketPrice);
+
+const underAge_Discount = (ticketPrice / 100) * 20;
+const overAge_Discount = (ticketPrice / 100) * 40;
 const underAge_Ticket = ticketPrice - underAge_Discount;
-const overAge_Ticket = ticketPrice - overAge_Discount;
+const over_Ticket = ticketPrice - overAge_Discount;
+const selectAge = document.getElementById('age');
+const overAge = document.getElementById('overage');
+const underage = document.getElementById('underage');
+const over = document.getElementById('over_sixtyFive');
+
+
+if (selectAge == over) {
+    console.log(over_Ticket);
+} else if (selectAge == underage) {
+    console.log(underAge_Ticket);
+} else {
+    console.log(ticketPrice);
+
+}
