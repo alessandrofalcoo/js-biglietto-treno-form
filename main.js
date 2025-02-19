519b768f229b850e77924a977e56c41a45998327
alessandrofalcoo <alefalco.1003@gmail.com>
2025-02-19 16:48:08 +0100

    add var Number to istructions in if statement add toFixed method to the tickets

--- main.js
+++ main.js
@@ -1,27 +1,28 @@
 const nameEl = document.getElementById('name');
-const kmEl = Number(document.getElementById('km').value);
 
 
 const generate = document.querySelector('button')
 
 generate.addEventListener('click', function () {
 
+    const kmEl = Number(document.getElementById('km').value);
     const ticketPrice = 0.21 * kmEl;
+
     const underAge_Discount = (ticketPrice / 100) * 20;
-    const overAge_Discount = (ticketPrice / 100) * 40;
+    const over_Discount = (ticketPrice / 100) * 40;
     const underAge_Ticket = ticketPrice - underAge_Discount;
-    const over_Ticket = ticketPrice - overAge_Discount;
+    const over_Ticket = ticketPrice - over_Discount;
     let selectAge = document.getElementById('age').value;
     let overAge = document.getElementById('overage').value;
     let underage = document.getElementById('underage').value;
     let over = document.getElementById('over_sixtyFive').value;
 
-    if (selectAge = over) {
-        console.log(over_Ticket);
-    } else if (selectAge = underage) {
-        console.log(underAge_Ticket);
-    } else if (selectAge = overAge) {
-        console.log(ticketPrice);
+    if (selectAge == over) {
+        console.log(Number(over_Ticket.toFixed(2)));
+    } else if (selectAge == underage) {
+        console.log(Number(underAge_Ticket.toFixed(2)));
+    } else if (selectAge == overAge) {
+        console.log(Number(ticketPrice.toFixed(2)));
 
     }
 })
